4d881992cfd98929f11ff80b06e0fc66f439b348
mxntr <muratmer@gmail.com>
2018-04-10 16:03:39 +0300

    Local weather app with Fetch API

---
+++ local weather app/main.js
@@ -0,0 +1,30 @@
+let city = document.getElementById("city");
+let degree = document.getElementById("degree");
+let status = document.getElementById("status");
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+ let url1 = "https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + long
+ fetch(url1)
+ 	.then((resp) => resp.json()) // Transform the data into json
+   	.then(function(data){
+ 		
+
+ 		city.innerText = data.name;
+ 		degree.innerText = data.main.temp + " C";
+ 		degree.addEventListener("click", changeDegree)
+ 		function changeDegree(){
+if (degree.innerText === data.main.temp + " C"){
+	degree.innerText = (data.main.temp*1.8)+32 + " F"; 
+}else{
+	degree.innerText = data.main.temp + " C";
+}
+ 		};
+
+ 		status.innerHTML = "<img src=" + data.weather[0].icon + "/>";
+
+ 	})
+
+
+});
